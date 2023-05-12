70b5a3fecd7d78beb78a4c6e2b93aec2058f1009
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-05-12 14:44:57 -0700

    .

--- main_test.go
+++ main_test.go
@@ -5,31 +5,31 @@
 package main_test
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
+
+	"cloudeng.io/os/executil"
 )
 
 var iduCommand string
 
 func buildIDU(tmpDir string) (string, error) {
-	bin := filepath.Join(tmpDir, "idu")
-	cmd := exec.Command("go", "build", "-o", bin, "cloudeng.io/cmd/idu")
-	out, err := cmd.CombinedOutput()
+	bin, err := executil.GoBuild(context.Background(), filepath.Join(tmpDir, "idu"), "cloudeng.io/cmd/idu")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "build failed: %v\n%s\n", strings.Join(cmd.Args, " "), out)
 		return "", err
 	}
+	fmt.Printf("BIN: %v\n", bin)
 	return bin, nil
 }
 
 func TestMain(m *testing.M) {
-	tmpDir, _ := ioutil.TempDir("", "idu")
+	tmpDir, _ := os.MkdirTemp("", "idu")
 	bin, err := buildIDU(tmpDir)
 	if err != nil {
 		os.RemoveAll(tmpDir)
@@ -42,34 +42,12 @@ func TestMain(m *testing.M) {
 }
 
 func runIDU(args ...string) (string, error) {
-	dir := filepath.Dir(iduCommand)
-	fmt.Printf("DIR: %v: cmd %v\n", dir, iduCommand)
-	fi, err := os.Stat(dir)
-	if err != nil {
-		fmt.Printf("STAT: %v\n", fi.Name())
-	} else {
-		fmt.Printf("ERR: %v\n", err)
-		entries, err := os.ReadDir(dir)
-		if err != nil {
-			fmt.Printf("ERR: %v\n", err)
-		} else {
-			for _, e := range entries {
-				fmt.Printf("ENTRY: %v\n", e.Name())
-			}
-		}
-	}
-
-	fi, err = os.Stat(iduCommand)
-	if err != nil {
-		fmt.Printf("STAT CMD: %v\n", fi.Name())
-	} else {
-		fmt.Printf("ERR CMD: %v\n", err)
-	}
-
 	cmd := exec.Command(iduCommand, args...)
-	fmt.Printf("CMD: %#v\n", cmd)
 	out, err := cmd.CombinedOutput()
-	return string(out), fmt.Errorf("%v: %v", strings.Join(cmd.Args, " "), err)
+	if err != nil {
+		return string(out), fmt.Errorf("%v: %v", strings.Join(cmd.Args, " "), err)
+	}
+	return string(out), nil
 }
 
 func containsAnyOf(got string, expected ...string) error {
@@ -83,10 +61,7 @@ func containsAnyOf(got string, expected ...string) error {
 }
 
 func TestHelp(t *testing.T) {
-	out, err := runIDU("help")
-	if err != nil {
-		t.Fatal(err)
-	}
+	out, _ := runIDU("help") // will return exit status 1 for help.
 	base := []string{
 		"idu: analyze file systems to create a database of per-file and aggregate size",
 		"errors - list the contents of the errors database",
@@ -95,15 +70,15 @@ func TestHelp(t *testing.T) {
 	if err := containsAnyOf(out, base...); err != nil {
 		t.Fatal(err)
 	}
-	out, _ = runIDU("--help")
+	out, _ = runIDU("--help") // will return exit status 1 for --help
 	if err := containsAnyOf(out, base...); err != nil {
 		t.Fatal(err)
 	}
-	err = containsAnyOf(out, "[--config=$HOME/.idu.yml --exit-profile= --gcpercent=50 --h=true --http= --units=decimal --v=0]")
+	err := containsAnyOf(out, "[--config=$HOME/.idu.yml --exit-profile= --gcpercent=50 --h=true --http= --units=decimal --v=0]")
 	if err != nil {
 		t.Fatal(err)
 	}
-	out, _ = runIDU("help", "analyze")
+	out, _ = runIDU("help", "analyze") // will return exit status 1 for help.
 
 	err = containsAnyOf(out, "Usage of command analyze: analyze the file system to build a database of file counts, disk usage etc",
 		" -incremental")
