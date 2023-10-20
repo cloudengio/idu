83d57ea1dc32223823d78b43133f5579a8f27552
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2023-10-20 09:53:03 -0700

    .

---
+++ lstat.go
@@ -0,0 +1,127 @@
+// Copyright 2020 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"path/filepath"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"cloudeng.io/cmd/idu/internal"
+	"cloudeng.io/cmd/idu/internal/config"
+	"cloudeng.io/file"
+	"cloudeng.io/file/filewalk"
+	"cloudeng.io/sync/errgroup"
+	"cloudeng.io/sync/syncsort"
+)
+
+type lstatIssuer struct {
+	cfg config.Prefix
+	fs  filewalk.FS
+	w   *walker
+	pt  *progressTracker
+
+	statsMu sync.Mutex
+	min     time.Duration
+	max     time.Duration
+}
+
+type lstatResult struct {
+	info file.Info
+	err  error
+}
+
+func newLStatIssuer(w *walker, cfg config.Prefix, fs filewalk.FS) *lstatIssuer {
+	return &lstatIssuer{
+		cfg: cfg,
+		fs:  fs,
+		w:   w,
+		pt:  w.pt,
+	}
+}
+
+func (lsi *lstatIssuer) updateLatencyStats(start time.Time) {
+	lsi.pt.updateStatLatency(start)
+	lsi.statsMu.Lock()
+	defer lsi.statsMu.Unlock()
+	took := time.Since(start)
+	lsi.min = min(lsi.min, took)
+	lsi.max = max(lsi.max, took)
+
+}
+
+func (lsi *lstatIssuer) lstat(ctx context.Context, state *prefixState, prefix, filename string) (file.Info, error) {
+	start := time.Now()
+	info, err := lsi.fs.LStat(ctx, filename)
+	lsi.updateLatencyStats(start)
+	if err != nil {
+		internal.Log(ctx, internal.LogError, "stat error", "prefix", lsi.cfg.Prefix, "file", filename, "error", err)
+		lsi.w.dbLog(ctx, filename, []byte(err.Error()))
+	}
+	return info, err
+}
+
+func (lsi *lstatIssuer) lstatContents(ctx context.Context, state *prefixState, prefix string, contents []filewalk.Entry) (file.InfoList, error) {
+	if len(contents) < 10 {
+		return lsi.syncIssue(ctx, state, prefix, contents)
+	}
+	return lsi.asyncIssue(ctx, state, prefix, contents, lsi.w.cfg.ConcurrentStats)
+}
+
+func (lsi *lstatIssuer) syncIssue(ctx context.Context, state *prefixState, prefix string, contents []filewalk.Entry) (file.InfoList, error) {
+	var children file.InfoList
+	for _, entry := range contents {
+		filename := lsi.fs.Join(prefix, entry.Name)
+		info, err := lsi.lstat(ctx, state, prefix, filename)
+		if err != nil {
+			continue
+		}
+		if entry.IsDir() {
+			atomic.AddInt64(&state.nchildren, 1)
+			children = append(children, info)
+		} else {
+			atomic.AddInt64(&state.nfiles, 1)
+		}
+		state.current.AppendInfo(info)
+	}
+	return children, nil
+}
+
+func (lsi *lstatIssuer) asyncIssue(ctx context.Context, state *prefixState, prefix string, contents []filewalk.Entry, concurrency int) (file.InfoList, error) {
+	g := &errgroup.T{}
+	g = errgroup.WithConcurrency(g, min(concurrency, len(contents)))
+	ch := make(chan syncsort.Item[lstatResult], len(contents))
+	seq := syncsort.NewSequencer(ctx, ch)
+	for _, entry := range contents {
+		name := entry.Name
+		item := seq.NextItem(lstatResult{})
+		g.Go(func() error {
+			filename := filepath.Join(prefix, name)
+			info, err := lsi.lstat(ctx, state, prefix, filename)
+			item.V = lstatResult{info, err}
+			ch <- item
+			return nil
+		})
+	}
+	g.Wait()
+	close(ch)
+	var children file.InfoList
+	for seq.Scan() {
+		res := seq.Item()
+		if res.err != nil {
+			continue
+		}
+		if res.info.IsDir() {
+			atomic.AddInt64(&state.nchildren, 1)
+			children = append(children, res.info)
+		} else {
+			atomic.AddInt64(&state.nfiles, 1)
+		}
+		state.current.AppendInfo(res.info)
+	}
+	return children, seq.Err()
+}
---
+++ testdata_test.go
@@ -0,0 +1,176 @@
+// Copyright 2020 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"cloudeng.io/errors"
+	"cloudeng.io/sys/windows/win32testutil"
+)
+
+type testtree struct {
+	root         string
+	depth        int
+	breadth      int
+	nfiles       int
+	baseContent  []string
+	addedContent []string
+}
+
+func (tt *testtree) createLevel(parent string, level int) ([]string, error) {
+	created := []string{}
+	contents := []byte{0x1}
+	for i := 0; i < tt.nfiles; i++ {
+		filename := filepath.Join(parent, fmt.Sprintf("f%02v-%02v", level, i))
+		if err := os.WriteFile(filename, contents, 0666); err != nil {
+			return nil, err
+		}
+		contents = append(contents, 0x1)
+		created = append(created, filename)
+	}
+
+	dirs := []string{}
+	for i := 0; i < tt.breadth; i++ {
+		dirname := filepath.Join(parent, fmt.Sprintf("d%02v-%02v", level, i))
+		dirs = append(dirs, dirname)
+		if err := os.Mkdir(dirname, 0777); err != nil {
+			return nil, err
+		}
+		created = append(created, dirname)
+	}
+
+	j := filepath.Join
+	errs := errors.M{}
+	err := os.Mkdir(j(parent, "d-inaccessible-dir"), 0000)
+	errs.Append(err)
+	err = win32testutil.MakeInaccessibleToOwner(j(parent, "d-inaccessible-dir"))
+	errs.Append(err)
+	err = os.Symlink(j("f0"), j(parent, "f-soft-link-f0"))
+	errs.Append(err)
+	err = os.Symlink("nowhere", j(parent, "f-soft-link-f1"))
+	errs.Append(err)
+	err = os.WriteFile(j(parent, "f-inaccessible-file"), []byte{'1', '2', '3'}, 0000)
+	errs.Append(err)
+	err = win32testutil.MakeInaccessibleToOwner(j(parent, "f-inaccessible-file")) // windows.
+
+	created = append(created,
+		j(parent, "d-inaccessible-dir"),
+		j(parent, "f-soft-link-f0"),
+		j(parent, "f-soft-link-f1"),
+		j(parent, "f-inaccessible-file"),
+	)
+	return created, errs.Err()
+}
+
+// createTree creates a tree of files and directories with the specified
+// depth and breadth. Each directory will contain nfiles files.
+// In additon, inaccessible files and directories are created, as are soft links.
+func (tt *testtree) createTree(parent string, level int, created []string) ([]string, error) {
+	if tt.depth == level {
+		return created, nil
+	}
+	d, err := tt.createLevel(parent, level)
+	if err != nil {
+		return nil, err
+	}
+	created = append(created, d...)
+	for i := 0; i < tt.breadth; i++ {
+		parent := filepath.Join(parent, fmt.Sprintf("d%02v-%02v", level, i))
+		c, err := tt.createTree(parent, level+1, created)
+		if err != nil {
+			return nil, err
+		}
+		created = c
+	}
+	return created, nil
+}
+
+func (tt *testtree) createBase() error {
+	c, err := tt.createTree(tt.root, 0, []string{tt.root})
+	tt.baseContent = c
+	return err
+}
+
+func (tt *testtree) base() []string {
+	return tt.baseContent
+}
+
+func (tt *testtree) createAdditional(depth, nfiles, ndirs int) error {
+	p := tt.root
+	for i := 0; i < depth; i++ {
+		p = filepath.Join(p, fmt.Sprintf("d%02v-00", i))
+	}
+	fileOffset, dirOffset := 0, 0
+	if depth < tt.depth {
+		fileOffset, dirOffset = tt.nfiles, tt.breadth
+	}
+	contents := make([]byte, fileOffset)
+	for i := fileOffset; i < fileOffset+nfiles; i++ {
+		filename := filepath.Join(p, fmt.Sprintf("f%02v-%02v", depth, i))
+		if err := os.WriteFile(filename, contents, 0666); err != nil {
+			return err
+		}
+		contents = append(contents, 0x1)
+		tt.addedContent = append(tt.addedContent, filename)
+	}
+	for i := dirOffset; i < dirOffset+ndirs; i++ {
+		dirname := filepath.Join(p, fmt.Sprintf("d%02v-%02v", depth, i))
+		if err := os.Mkdir(dirname, 0777); err != nil {
+			return err
+		}
+		tt.addedContent = append(tt.addedContent, dirname)
+	}
+	return nil
+}
+
+func (tt *testtree) additional() []string {
+	return tt.addedContent
+}
+
+func numDirsAndFiles(paths []string) (d, f int64) {
+	for _, a := range paths {
+		switch filepath.Base(a)[0] {
+		case 'd':
+			d++
+		case 'f':
+			f++
+		}
+	}
+	return
+}
+
+func (tt *testtree) deletionAdditonal() error {
+	// remove files first.
+	for _, a := range tt.addedContent {
+		b := filepath.Base(a)
+		if b[0] == 'f' {
+			if err := os.Remove(a); err != nil {
+				return err
+			}
+		}
+	}
+	for _, a := range tt.addedContent {
+		if filepath.Base(a)[0] == 'f' {
+			continue
+		}
+		if err := os.Remove(a); err != nil {
+			return err
+		}
+	}
+	tt.addedContent = nil
+	return nil
+}
+
+func newTestTree(parent string, depth, breadth, nfiles int) *testtree {
+	return &testtree{
+		root:    filepath.Join(parent, "d-testree"),
+		depth:   depth,
+		breadth: breadth,
+		nfiles:  nfiles,
+	}
+}
