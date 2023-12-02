5089b4f473ab7d4df7088104bca8fa057979588a
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-12-01 16:23:58 -0800

    .

--- internal/prefixinfo/userinfo_windows.go
+++ internal/prefixinfo/userinfo_windows.go
@@ -18,7 +18,9 @@ func userGroupID(fi file.Info) (userID, groupID uint32, ok bool) {
 	if u, ok := fi.Sys().(*userInfo); ok {
 		return u.uid, u.gid, true
 	}
-	return 0, 0, false
+	// TODO: implement user/group in some form for windows, for now just
+	// pretend that the user and group are zero.
+	return 0, 0, true
 }
 
 func sysUserGroupID(uid, gid uint32) any {
